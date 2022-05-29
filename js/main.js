f1cb3b1d5573795a061809c4039a5fbebb6837e1
thomas19860416 <thomas.horvath0416@gmail.com>
2022-05-29 18:16:45 +0200

    jo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-console.log("hello this is a consol masseg");
+function calcAmount() {
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount.input']");
+    let showAmount = document.querySelector("span.show-amount");
+    let amountNumber = parseInt(amountInput.value);
+    
+    if (amountNumber > 10) {
+        alert("Maximum 10 terméket vásárolhat");
+        return;
+
+    } else if (amountNumber < 1) {
+        alert("Minimum egy terméket kell vásárolnia");
+    }
+    else {
+        let amount = parseInt(amountInput.value) * price;
+
+        showAmount.innerHTML = amount;
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
