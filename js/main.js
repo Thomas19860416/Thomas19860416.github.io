420d02c0b1832675da4ffde38030b4a65fabe82f
thomas19860416 <thomas.horvath0416@gmail.com>
2022-05-30 20:52:20 +0200

    jo

--- js/main.js
+++ js/main.js
@@ -1,28 +1,25 @@
 function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount.input']");
-    let showAmount = document.querySelector("span.show-amount");
     let amountNumber = parseInt(amountInput.value);
-    if (isNaN(amountNumber)){
-        amountNumber = 0;
-    }
+    amountNumber =isNaN(amountNumber) ? 0 : amountNumber;
+    
+    showSumPrice(price, amountNumber);
+}
+
+function showSumPrice(price, amountNumber) {
+    let showAmount = document.querySelector("span.show-amount");
     
     if (amountNumber > 10) {
         alert("Maximum 10 terméket vásárolhat");
-        return;
-
-    } else if (amountNumber < 1) {
+  } else if (amountNumber < 1) {
         alert("Minimum egy terméket kell vásárolnia");
     }
     else {
-        let amount = parseInt(amountInput.value) * price;
-
+        let amount = amountNumber * price;
+    
         showAmount.innerHTML = amount;
     }
-
-
-
-
 
 
 }
---
+++ js/scope.js
@@ -0,0 +1,9 @@
+var globalName ="Joe";
+
+function something() {
+    var globalName = "Jack";
+    console.log(globalName);
+}
+something();
+
+console.log(globalName);
